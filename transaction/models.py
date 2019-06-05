81e4ace971cd6987a7081ff5ed830c4b733b7dc7
Emmanuel <emmaneulukwuegbu2016@gmail.com>
2019-06-04 18:49:02 -0700

    updated transaction table

--- transaction/models.py
+++ transaction/models.py
@@ -6,15 +6,15 @@ from django.conf import settings
 class TransactionHistory(models.Model):
     user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
     related_name='transaction_history')
-    client_id = models.ForeignKey('invoice.ClientInfo', on_delete=models.CASCADE, 
-    related_name='client_transact_history')
+    #client_id = models.ForeignKey('invoice.ClientInfo', on_delete=models.CASCADE, 
+    #related_name='client_transact_history', null=True)
     invoice_id = models.IntegerField()
     transact_amount = models.DecimalField(decimal_places=2, max_digits=20)
-    transact_data = models.DateTimeField(auto_now_add=True)
+    transact_date = models.DateTimeField(auto_now_add=True)
     description = models.CharField(max_length=200)
 
     class Meta:
-        ordering = ('-transact_amount',)
+        ordering = ('-transact_date',)
         
 
     def __str__(self):
